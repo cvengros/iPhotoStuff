554b0e9c5341565f91a4e1a3efd3e1ce61c9b963
Petr Cvengros <petr@cvengros.cz>
2018-07-18 23:09:33 +0200

    a bit of a refactor

--- fix_directory_names.sh
+++ fix_directory_names.sh
@@ -5,17 +5,19 @@ for WEIRD_DATE in *; do
   if [ -d "$WEIRD_DATE" ]; then
     # try converting to a better date
     BETTER_DATE=`date -jf "%B %d, %Y" "${WEIRD_DATE}" '+%Y-%m-%d' 2> /dev/null`
-    if [ $? -eq 0 ]; then
-      if [ -d "$BETTER_DATE" ]; then
-          echo "$BETTER_DATE already exists, skipping $WEIRD_DATE"
-      else
-        # converting successful and directory doesn't exist, so rename it
-        echo "Renaming $WEIRD_DATE to $BETTER_DATE"
-        mv "$WEIRD_DATE" "$BETTER_DATE"
-      fi
-    else
+    if [ $? -ne 0 ]; then
       # convert didn't work
       echo "Skipping $WEIRD_DATE"
+      continue
     fi
+
+    if [ -d "$BETTER_DATE" ]; then
+      echo "$BETTER_DATE already exists, skipping $WEIRD_DATE"
+      continue
+    fi
+
+    # converting successful and directory doesn't exist, so rename it
+    echo "Renaming $WEIRD_DATE to $BETTER_DATE"
+    mv "$WEIRD_DATE" "$BETTER_DATE"
   fi
 done
